d6df0b00d427ee835481e459b6e6d0528cbc48cf
Prayag Bhatt <prayagbhatt2003@gmail.com>
2026-01-04 14:28:34 +0530

    [feat-main]: add main function logic

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Prayag2003/consistent-hashing
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	replicas := 100
+	if len(os.Args) > 1 {
+		if r, err := strconv.Atoi(os.Args[1]); err == nil {
+			replicas = r
+		}
+	}
+	fmt.Println("Virtual nodes per server: ", replicas)
+
+	ring := NewHashRing(replicas)
+	servers := []string{
+		"10.0.0.1",
+		"10.0.0.2",
+		"10.0.0.3",
+	}
+
+	for _, s := range servers {
+		ring.AddServer(s)
+	}
+
+	var requests []string
+	for i := range 10000 {
+		requests = append(requests, "192.168.1."+strconv.Itoa(i))
+	}
+
+	fmt.Println("\n\nLoad Distribution before adding server:")
+	printLoad(assignRequests(ring, requests))
+
+	ring.AddServer("10.0.0.4")
+	fmt.Println("\n\nLoad Distribution after adding server:")
+	printLoad(assignRequests(ring, requests))
+}
